6d71b03ac1be98cb3a84f87d0c084f1ad9fc5fd5
Jürgen Schoenemeyer <108626845+jschoen42@users.noreply.github.com>
2024-12-02 17:50:52 +0100

    initial commit

---
+++ js/postMessageChild.js
@@ -0,0 +1,24 @@
+const child_domain  = "http://tutorial.test";
+const parent_domain = "http://vendor.test";
+
+// Send message to parent
+
+function sendMessageToParent( message ) {
+  const myMessage = {
+    message: message,
+    timestamp: new Date().toISOString()
+  }
+
+  parent.postMessage(myMessage, parent_domain);
+  console.log("send message to parent2, event.data");
+}
+
+// Receive message from parent
+
+window.addEventListener('message', function(event) {
+  if ( new URL(event.origin).host != new URL(parent_domain).host ) {
+    return;
+  }
+
+  console.log(`from parent -> event.data`, event.data)
+}, false);
---
+++ js/postMessagesParent.js
@@ -0,0 +1,27 @@
+const child_domain  = "http://tutorial.test";
+const parent_domain = "http://vendor.test";
+
+const iframe = document.getElementById('iframe');
+
+// Send message to iframe
+
+function sendMessageToIframe( message ) {
+  const myMessage = {
+    message: message,
+    timestamp: new Date().toISOString()
+  }
+
+  iframe.contentWindow.postMessage(myMessage, child_domain);
+  console.log("send message to child", myMessage);
+}
+
+// Receive message from iframe
+
+window.addEventListener('message', function(event) {
+  if ( new URL(event.origin).host != new URL(child_domain).host ) {
+    return;
+  }
+
+  console.log("from child -> event.data", event.data )
+  this.alert( event.data.message )
+}, false);
